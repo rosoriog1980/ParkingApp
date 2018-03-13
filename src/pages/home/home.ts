3859b206a278b33c506e8e29b936e6d46768f998
PSLCOL\rosoriog <rosoriog@psl.com.co>
2018-03-13 15:57:31 -0500

    Home Page Changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,13 +10,9 @@ import { SingletonCacheProvider } from '../../providers/singleton-cache/singleto
 })
 export class HomePage {
 
-  parkingLots: any;
-  availableLots: any;
-  availableLotsQuantity: string;
   token: string;
-  parkingMessage = "";
   userName: string;
-
+  branchOffice: string;
 
   constructor(public navCtrl: NavController,
     private parkingService: ParkingProvider,
@@ -32,44 +28,12 @@ export class HomePage {
 
     this.singletonCache.getUser()
     .then(usr => {
-      var user = usr;
-      this.userName = user["userName"];
+      this.userName = usr["userName"];
+      this.branchOffice = usr["branchOfficeId"]["officeName"];
     });
   }
 
-  getAvailableParkingLots(){
-    this.parkingService.getParkingLots(this.token).then(data =>{
-      this.parkingLots = data;
-
-      this.availableLots = this.find_in_object(this.parkingLots, {parkingStatus: 'AVAILABLE'});
-
-
-      var floors = [];
-
-      for(var lot in this.availableLots){
-        var floorNumber = this.availableLots[lot]["floorNumber"];
-        if(floors.find(function (obj) {return obj == floorNumber}) == undefined){
-          floors.push(floorNumber)
-        }
-      }
-
-      for(var floor in floors){
-        var availableLotsInFloor = this.availableLots.filter(function( obj ) {
-          return obj.floorNumber == floors[floor];
-        });
-
-        if(availableLotsInFloor.length == 1)
-          this.parkingMessage = this.parkingMessage.concat(", hay " + availableLotsInFloor.length + " parqueo disponible en el sotano " + floors[floor]);
-
-        if(availableLotsInFloor.length > 1)
-        this.parkingMessage = this.parkingMessage.concat(", hay " + availableLotsInFloor.length + " parqueos disponibles en el sotano " + floors[floor]);
-      }
-    })
-  }
-
-
   ionViewWillEnter(){
-    this.getAvailableParkingLots();
   }
 
   find_in_object(my_object, my_criteria){
